feccb91c9e5cb31eb14014a72fc0494e42e1e138
aatish <aatish@vacationlabs.com>
2017-12-08 18:54:03 +0530

    Rotation added

---
+++ rotational.hs
@@ -0,0 +1,19 @@
+--http://exercism.io/exercises/haskell/rotational-cipher/readme
+import Data.Char
+import Data.List
+merge :: [x] -> [x] -> [x]
+merge [] [] = []
+merge x [] = x
+merge [] x = x
+merge (x:xs) (y:ys) = x : y : merge xs ys
+
+createmap = ['a'..'z']
+geti x xs k= createmap !! ((head (Data.List.elemIndices x xs))+k)
+
+toString :: Char -> String
+toString c = [c]
+rotation [] k = ""
+rotation x k = (toString(geti (head x) x k)) ++(rotation (tail x) k)
+main :: IO()
+main = do
+    print (rotation "bbbb" 3)
