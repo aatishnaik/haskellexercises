0910cc537e14798a608c07c3e19b9e28c2c2371c
aatish <aatish@vacationlabs.com>
2017-12-13 13:12:38 +0530

    sum of squares fix

--- sumsq.hs
+++ sumsq.hs
@@ -1,3 +1,10 @@
-
 sqr x = x * x
 sumsq n = sqr (sum [1..n])
+
+sqsumcal [] = 0
+sqsumcal x = sqr (head x) + sqsumcal (tail x)
+
+sqsum a = sqsumcal [1..a]
+main :: IO()
+main = do
+    print ((sumsq 100) - (sqsum 100))
