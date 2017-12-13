77b2dc639ec8f23ea73beec383a81089330d2376
aatish <aatish@vacationlabs.com>
2017-12-13 12:21:33 +0530

    leapfix

--- leap.hs
+++ leap.hs
@@ -1,10 +1,11 @@
 --http://exercism.io/exercises/haskell/leap/readme
 checkLeap yr = if (yr `mod` 4) == 0
     then if (yr `mod` 100) /= 0
-            then putStrLn "Leap"
+            then True
         else if (yr `mod` 400) /= 0
-            then putStrLn "not Leap"
-            else putStrLn "Leap"
-        else putStrLn "not Leap"
+            then False
+            else True
+        else False
+main :: IO()
 main = do
-    checkLeap (2015)
+    print (checkLeap (2015))
