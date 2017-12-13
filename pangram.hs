af7f07f469bf2a916069c90024ac99b8e9150aa9
aatish <aatish@vacationlabs.com>
2017-12-13 12:42:17 +0530

    Pangram fix

--- pangram.hs
+++ pangram.hs
@@ -1,8 +1,20 @@
 --http://exercism.io/exercises/haskell/pangram/readme
+import Data.Char
+import Data.List
 
-checkpan b = if ((filter (=='a') b) == "a") && ((filter (=='b') b) == "b") && ((filter (=='c') b) == "c") && ((filter (=='d') b) == "d") && ((filter (=='e') b) == "e") && ((filter (=='f') b) == "f") && ((filter (=='g') b) == "g") && ((filter (=='h') b) == "h") && ((filter (=='i') b) == "i") && ((filter (=='j') b) == "j") && ((filter (=='k') b) == "k") && ((filter (=='l') b) == "l") && ((filter (=='m') b) == "m") && ((filter (=='n') b) == "n") && ((filter (=='o') b) == "o") && ((filter (=='p') b) == "p") && ((filter (=='q') b) == "q") && ((filter (=='r') b) == "r") && ((filter (=='s') b) == "s") && ((filter (=='t') b) == "t") && ((filter (=='u') b) == "u") && ((filter (=='v') b) == "v") && ((filter (=='w') b) == "w") && ((filter (=='x') b) == "x") && ((filter (=='y') b) == "y") && ((filter (=='z') b) == "z")
-    then "Pangram"
-    else "Not Pangram"
+createmap = ['a'..'z']
+getnext x = createmap !! ((head (Data.List.elemIndices x createmap))+1)
+
 main :: IO()
 main = do
     print (checkpan "abcdefghijklmnopqrstuvwxyz")
+checkp x 'z' = True
+checkp x y = if (y `elem` x)
+    then checkp x (getnext y)
+    else False
+
+checkpan x = checkp x 'a'
+
+main :: IO()
+main = do
+    checkpan "abcdefghijklmnopqrstuvwxyzabc"
