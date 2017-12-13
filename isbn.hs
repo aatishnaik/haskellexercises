2a3e9d9ab68e0feaa91fb5d3197c464aa87728e0
aatish <aatish@vacationlabs.com>
2017-12-13 17:36:29 +0530

    Isbn

---
+++ isbn.hs
@@ -0,0 +1,39 @@
+import Data.List
+import Data.Char
+
+cformat x = if (length x) == 13
+    then if (x!!1) == '-'
+        then if (x!!5) == '-'
+            then if (x!!11) == '-'
+                then True
+                else False
+            else False
+        else False
+    else False
+toInt :: Char -> Int
+toInt x = digitToInt x
+
+cdigit x 1 = cdigit (tail x) 2
+cdigit x 5 = cdigit (tail x) 6
+cdigit x 11 = cdigit (tail x) 12
+cdigit x 12 = if isDigit (head x)
+    then True
+    else False
+cdigit [] n = True
+cdigit x n = if isDigit (head x)
+    then cdigit (tail x) (n+1)
+    else False
+calsum x 0 = 0
+calsum [] n = 0
+calsum x n 
+    | n > 11 = ((toInt (head x)) * (n-1)) + (calsum (tail x) (n-1))
+    | n > 5 =  if (n == 11)
+        then calsum (tail x) (n-1)
+        else ((toInt (head x)) * n) + (calsum (tail x) (n-1))
+    | otherwise =  if (n == 1) || (n == 5)
+        then calsum (tail x) (n-1)
+        else ((toInt (head x)) * (n+1)) + (calsum (tail x) (n-1))
+
+checkisbn x = if (cformat x) && (cdigit x 0)
+    then ((calsum x 12) `mod` 11) == 0
+    else False
