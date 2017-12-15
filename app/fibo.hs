a13ffc6e290b39eeaf377db2eb81b99cc93e2a69
aatish <aatish@vacationlabs.com>
2017-12-15 11:35:17 +0530

    fibofix

--- app/fibo.hs
+++ app/fibo.hs
@@ -3,13 +3,23 @@ fibo 0 = 0
 fibo 1 = 1
 fibo n = fibo (n-1) + fibo (n-2)
 
-fibonacci :: [Integer]
-fibonacci = map fibo [1..20]
+fibonacci :: Integer -> [Integer]
+fibonacci n = map fibo [1..n]
+
 fibsum :: [Integer] -> Integer
 fibsum [x] = x
 fibsum xs = if ((head xs) `mod` 2) == 0
     then head xs + fibsum (tail xs)
     else fibsum (tail xs)
+
+--one func for task
+evenfib :: Integer -> Integer -> Integer -> Integer
+evenfib x y ttl 
+    |(x+y) <= 11000 = if (y `mod` 2) == 0
+        then ttl + (evenfib y (x+y) (y+ttl))
+        else evenfib y (x+y) ttl
+    | otherwise = 0
+
 main :: IO()
 main =
-    print (fibsum fibonacci)
+    print (fibsum (fibonacci 21))
--- app/rail.hs
+++ app/rail.hs
@@ -1,2 +1,10 @@
-rail :: String -> Int -> [String]
-rail x n = 
+import Data.Char
+import Data.List
+
+keydiff = [-1..10]
+rail :: String -> Int -> Int -> [String]
+
+
+rail x n i = if i < n
+    then head x
+    else
