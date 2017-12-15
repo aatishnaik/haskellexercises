0b8c0ad241b18c51a6604f70bfb9874eecbd3886
aatish <aatish@vacationlabs.com>
2017-12-15 11:59:01 +0530

    multiple fix

--- app/multiples.hs
+++ app/multiples.hs
@@ -1,4 +1,4 @@
-checkmul :: Int -> Int
+checkmul :: Integer -> Integer
 checkmul x = if ( x `mod` 3) == 0
     then x
     else if ( x `mod` 5) == 0
@@ -7,9 +7,14 @@ checkmul x = if ( x `mod` 3) == 0
 
 tolist x = [x]
 
-findmul :: [Int] -> [Int]
-findmul [] = [0]
-findmul xs = (tolist (checkmul (head xs))) ++ (findmul (tail xs))
+findmul :: Integer -> Integer
+--findmul xs = (tolist (checkmul (head xs))) ++ (findmul (tail xs))
+findmul n 
+        |n < 10 = if (checkmul n) == n
+            then (n + (findmul (n+1)))
+            else findmul (n+1)
+        |otherwise = 0
+
 main :: IO()
-main = do
-    print (sum (findmul [1..1000-1]))
+main = print (findmul 0)
+--    print (sum (findmul [1..1000-1]))
