89d5d3bdf23df4eb74bf3e80dd0cf6ef69a4452d
aatish <aatish@vacationlabs.com>
2017-12-15 12:51:29 +0530

    pangram fix

--- app/pangram.hs
+++ app/pangram.hs
@@ -4,20 +4,18 @@ import Data.List
 
 createmap :: String
 createmap = ['a'..'z']
-createmap1 :: String
-createmap1 = ['A'..'Z']
 getnext :: Char -> Char
 getnext x = createmap !! ((head (Data.List.elemIndices x createmap))+1)
-getnext1 :: Char -> Char
-getnext1 x = createmap1 !! ((head (Data.List.elemIndices x createmap1))+1)
-checkp :: String -> Char -> Char -> Bool
-checkp x _ 'Z'= True
-checkp x 'z' _= True
-checkp x y z= if (y `elem` x) || (z `elem` x)
-    then checkp x (getnext y) (getnext1 z)
+
+clowercase :: String -> String
+clowercase str = map (toLower) str
+checkp :: String -> Char -> Bool
+checkp x 'z' = 'z' `elem` x
+checkp x y = if (y `elem` x)
+    then checkp x (getnext y)
     else False
 checkpan :: String -> Bool
-checkpan x = checkp x 'a' 'A'
+checkpan x = checkp (clowercase x) 'a'
 
 main :: IO()
 main = print (checkpan "Five quacking Zephyrs jolt my wax bed")
