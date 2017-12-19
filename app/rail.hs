f423a947991ade78bad66d00e219d75e8b4f6800
aatish <aatish@vacationlabs.com>
2017-12-19 17:56:14 +0530

    rfc in process. annotaion sent

--- app/rail.hs
+++ app/rail.hs
@@ -2,15 +2,17 @@ import Data.Char
 import Data.List
 
 diff key = (2 * (key - 1))
-
 indexarr key str = [1,(diff key)+1..(length str)]
 tostr :: Char -> String
 tostr x = [x]
+
 rmspace :: String -> String
 rmspace str = filter (/=' ') str
+
 getstr :: Int -> String -> [Int] -> String
 getstr _ _ [] = ""
 getstr key str index = tostr(str!!((head index)-1)) ++ (getstr key str (tail index))
+
 rail :: String -> Int -> String
 rail str k = getstr k (rmspace str) (indexarr k (rmspace str))
 main :: IO()
