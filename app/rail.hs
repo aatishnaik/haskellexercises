a4dc475a3cc0cd595fb7bd9a5974ac3fa4f23aea
aatish <aatish@vacationlabs.com>
2017-12-19 14:38:52 +0530

    rail space handle

--- app/rail.hs
+++ app/rail.hs
@@ -6,11 +6,13 @@ diff key = (2 * (key - 1))
 indexarr key str = [1,(diff key)+1..(length str)]
 tostr :: Char -> String
 tostr x = [x]
+rmspace :: String -> String
+rmspace str = filter (/=' ') str
 getstr :: Int -> String -> [Int] -> String
 getstr _ _ [] = ""
 getstr key str index = tostr(str!!((head index)-1)) ++ (getstr key str (tail index))
 rail :: String -> Int -> String
-rail str k = getstr k str (indexarr k str)
+rail str k = getstr k (rmspace str) (indexarr k (rmspace str))
 main :: IO()
 main =
-    print (rail "abcdefghijklmnopqrstuv" 2)
+    print (rail "WE ARE DISCOVERED FLEE AT ONCE" 3)
