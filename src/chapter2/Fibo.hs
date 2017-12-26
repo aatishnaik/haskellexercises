823617fb1e0a6d66a8efd0b6ba24cef977feb927
aatish <aatish@vacationlabs.com>
2017-12-26 14:14:30 +0530

    sumsq done

---
+++ src/chapter2/Fibo.hs
@@ -0,0 +1,4 @@
+module Chapter2.Fibo where
+import Data.Char
+import Data.List
+
---
+++ src/chapter2/SumSq.hs
@@ -0,0 +1,12 @@
+module Chapter2.SumSq where
+import Data.Char
+import Data.List
+
+sumsq :: Int -> Int
+sumsq n = sum (map (\c -> (c*c)) [1..n])
+
+sqsum :: Int -> Int
+sqsum n = (foldl' (\ttl x -> ttl + x) 0 [1..n])^2
+
+diff :: Int
+diff = (sqsum 1000) - (sumsq 1000)
