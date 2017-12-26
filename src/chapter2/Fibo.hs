22cc1b681801c997009014165d6fc9a91b35f16d
aatish <aatish@vacationlabs.com>
2017-12-26 17:37:25 +0530

    .

--- src/chapter2/Fibo.hs
+++ src/chapter2/Fibo.hs
@@ -6,5 +6,6 @@ fibo :: Integer -> Integer
 fibo 0 = 0
 fibo 1 = 1
 fibo n = fibo (n-1) + fibo (n-2)
+
 efibsum :: Integer -> Integer
 efibsum n = foldl' (\ttl x -> if ((x `mod` 2) == 0) then (ttl + x) else ttl) 0 (map fibo [1..n])
