c79d2d86e3fe5bb1b22c696cac3f4bb35a682aa0
aatish <aatish@vacationlabs.com>
2017-12-27 11:38:26 +0530

    chp 1 fixes done

--- src/chapter1/Isbn.hs
+++ src/chapter1/Isbn.hs
@@ -1,24 +1,27 @@
 module Chapter1.Isbn where
 import Data.List
 import Data.Char
-rmdash :: String -> String
-rmdash x = delete '-' (delete '-' (delete '-' x))
 
-cformat :: String -> String
-cformat x
-    | (length x) == 13 = if cdigit (rmdash x)
-        then rmdash x
-        else ""
-    | (length x) == 10 = if cdigit x
-        then x
-        else ""
-    | otherwise = ""
+rmdash :: String -> String
+rmdash str = if ('-' `elem` str)
+    then delete '-' (rmdash (delete '-' str))
+    else str
 
 cdigit :: String -> Bool
 cdigit [] = True
 cdigit x = if isDigit (head x)
     then cdigit (tail x)
     else False
+
+cformat :: String -> String
+cformat str
+    | (length str) > 10 = if cdigit (rmdash str)
+        then rmdash str
+        else ""
+    | (length str) == 10 = if cdigit str
+        then str
+        else ""
+    | otherwise = ""
 
 checkmod :: String -> Int -> Int
 checkmod x n
@@ -27,9 +30,9 @@ checkmod x n
 
 checkisbn :: String -> Bool
 checkisbn x = if (length (cformat x)) == 10
-        then ((checkmod x 10) `mod` 11) == 0
+        then ((checkmod (cformat x) 10) `mod` 11) == 0
         else False
 
 main :: IO()
 main =
-    print (checkisbn "3-598021508-8")
+    print (checkisbn "3-598-2150-88")
