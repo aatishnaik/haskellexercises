301249d584dbfa3fa9d16ddef95e5eecab09b642
aatish <aatish@vacationlabs.com>
2017-12-28 17:07:12 +0530

    Contact try

--- src/chapter2/ContactList.hs
+++ src/chapter2/ContactList.hs
@@ -2,12 +2,8 @@ module Chapter2.ContactList where
 import Data.Char
 import Data.List
 
-{-groupNamesByAlphabet :: [String] -> (Char, [String])
-groupNamesByAlphabet names = foldl' (\ (alphabet, collectedNames) name ->
-        if (length name > 0) && (toLower (head name) == (toLower alphabet))
-        then (alphabet, collectedNames ++ [name])
-        else (alphabet, collectedNames)
-    ) ('a', []) names-}
-
-groupNamesByAllAlphabets :: [String] -> [(Char, [String])]
-groupNamesByAllAlphabets names = filter (\ (alpha, collectedName) -> length (collectedName) > 0) (map (\alphabet -> groupNamesByAllAlphabets names alphabet) "abcdefghijklmnopqwxyz")
+groupNamesByAlphabet :: Char -> [String] -> (Char, [String])
+groupNamesByAlphabet alpha names = foldl' (\ (alphabet, collectedNames) name ->
+      if ((length name) > 0) && (toLower (head name) == (toLower alphabet))
+      then (alphabet, collectedNames ++ [name])
+      else (alphabet, collectedNames)) (alpha, []) names
