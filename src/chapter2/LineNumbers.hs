ad66039a12f0a3ec8153e154c203c2798e7bff03
aatish <aatish@vacationlabs.com>
2017-12-29 15:59:06 +0530

    lINE NUMBERS A C AND D PART DONE.

--- src/chapter2/LineNumbers.hs
+++ src/chapter2/LineNumbers.hs
@@ -2,11 +2,27 @@ module LineNumbers where
 import Data.Char
 import Data.List
 
-numlist n = map (\i -> show i) [1..n]
+numlist n = map (\i -> (show i)++":") [1..n]
 
-addnum :: [String] -> [(String,String)]
-addnum strlist = zip (numlist (length strlist)) (strlist)
+--using recursion
+add :: [String] -> [String] -> [String]
+add [n] [x] = [n++x]
+add numlist strlist = [(head numlist ++ head strlist)] ++ add (tail numlist) (tail strlist)
+addnum :: [String] -> [String]
+addnum strlist = add (numlist (length strlist)) strlist
+
+--using foldl'
+--addnum2 :: [String] -> [String]
+--addnum2 strlist = foldl' (\ str c -> str ++ c) (numlist (length strlist)) strlist
+
+--using zip
+addnum3 :: [String] -> [(String,String)]
+addnum3 strlist = zip (numlist (length strlist)) (strlist)
+
+--using zipWith
+addnum4 :: [String] -> [String]
+addnum4 strlist = zipWith (++) (numlist (length strlist)) strlist
 
 main :: IO()
-main = print (addnum ["abc","bbc","as","sa","ll"])
+main = print (addnum2 ["abc","bbc","as","sa","ll"])
 --main = print (numlist 20)
