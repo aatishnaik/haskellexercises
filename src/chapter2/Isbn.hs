01334f66273761aefd38aa4bd1f30dcca1d66d92
aatish <aatish@vacationlabs.com>
2017-12-29 17:28:55 +0530

    no chhange

--- src/chapter2/Isbn.hs
+++ src/chapter2/Isbn.hs
@@ -4,11 +4,9 @@ import Data.List
 
 rmdash :: String -> String
 rmdash str = filter (/='-') str
-
 cformat str = if (False `elem` (map (\c -> (isDigit c)) (rmdash str))) || (length (rmdash str) /= 10)
     then False
     else True
-
 checkisbn str = if (cformat str)
     then if ((foldl' (\val c -> (val+c)) 0 (map (\c -> (digitToInt c)) (rmdash str))) `mod` 11) == 0
         then True
@@ -16,4 +14,3 @@ checkisbn str = if (cformat str)
     else False
 
 --using zip
-
