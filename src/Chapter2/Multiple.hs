9867dfe11d98e037100ceaf3a23fc56b357c1575
aatish <aatish@vacationlabs.com>
2018-01-02 12:51:29 +0530

    multiple fix

--- src/Chapter2/Multiple.hs
+++ src/Chapter2/Multiple.hs
@@ -2,10 +2,12 @@ module Chapter2.Multiple where
 import Data.List
 
 --using filter
+multiple :: Int -> [Int]
 multiple num = filter (\x -> ((x `mod` 3) == 0) || ((x `mod` 5) == 0)) [1..num]
 
 --using foldl'
+summul :: Int -> Int
 summul n = foldl' (\ttl num -> if ((num `mod` 3) == 0) || ((num `mod` 5) == 0) then (ttl + num) else ttl) 0 [1..n]
 
 main ::IO()
-main = print (multiple 20)
+main = print (summul 20)
