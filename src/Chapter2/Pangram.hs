d326daa76ce3d2ba33b7cc8b0b7c0f0531967bd4
aatish <aatish@vacationlabs.com>
2018-01-02 13:25:06 +0530

    pangram fix

--- src/Chapter2/Pangram.hs
+++ src/Chapter2/Pangram.hs
@@ -3,22 +3,14 @@ import Data.Char
 import Data.List
 
 pangram :: String -> Bool
-pangram str = if (foldl' (\alphabets c -> delete (toLower c) alphabets) ['a'..'z'] str) == ""
-    then True
-    else False
+pangram str = (foldl' (\alphabets c -> delete (toLower c) alphabets) ['a'..'z'] str) == ""
 
---pangram2 :: String -> Bool
---pangram2 str = if (map (\c -> (filter (/=c) ['a'..'z'])) str) == ""
---    then True
---    else False
 pangram2 :: String -> Bool
-pangram2 str = if (foldl' (\test c -> test ++ (filter (==c) ['a'..'z'])) [] (toLow str)) == (filter (/=' ') (toLow str))
-    then True
-    else False
+pangram2 str = (foldl' (\test c -> test ++ (filter (==c) ['a'..'z'])) [] (toLow str)) == (filter (/=' ') (toLow str))
 
 toLow str = map (toLower) str
 
 pangram3 :: String -> Bool
-pangram3 str = if any (False==) (map (\c -> (any (c==) (toLow str))) ['a'..'z'])
-    then False
-    else True
+pangram3 str = (not)(any (False==) (map (\c -> (any (c==) (toLow str))) ['a'..'z']))
+main :: IO()
+main = print (pangram "abcdefghijklmnopqrstuvwxyz")
