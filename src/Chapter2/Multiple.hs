daef3f4bab4a87b3fac3197c6757a88a3df6da4c
aatish <aatish@vacationlabs.com>
2018-01-02 12:49:34 +0530

    multiple fix

--- src/Chapter2/Multiple.hs
+++ src/Chapter2/Multiple.hs
@@ -1,9 +1,11 @@
 module Chapter2.Multiple where
 import Data.List
-check num = if ((num `mod` 3) == 0) || ((num `mod` 5) == 0)
-    then True
-    else False
-multiple list = filter check list
+
+--using filter
+multiple num = filter (\x -> ((x `mod` 3) == 0) || ((x `mod` 5) == 0)) [1..num]
 
 --using foldl'
 summul n = foldl' (\ttl num -> if ((num `mod` 3) == 0) || ((num `mod` 5) == 0) then (ttl + num) else ttl) 0 [1..n]
+
+main ::IO()
+main = print (multiple 20)
