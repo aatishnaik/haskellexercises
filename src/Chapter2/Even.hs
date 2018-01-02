ba594b6dcdf0047c29a6bc61812d304d531f1b07
aatish <aatish@vacationlabs.com>
2018-01-02 12:32:35 +0530

    even fix

--- src/Chapter2/Even.hs
+++ src/Chapter2/Even.hs
@@ -1,8 +1,13 @@
 module Chapter2.Even where
 import Data.List
+check :: Int -> Bool
 check num = if (num `mod` 2) == 0
     then True
     else False
-even n = filter check [1..n]
 
-even2 n = foldl' (\evenList x -> if ((x `mod` 2) == 0) then (evenList ++ [x]) else (evenList ++ [])) [] [1..n]
+even :: Int -> [Int]
+even n = filter check [1..n]
+even2 :: Int -> [Int]
+even2 n = foldl' (\evenList x -> if ((x `mod` 2) == 0) then (evenList ++ [x]) else (evenList)) [] [1..n]
+main ::IO()
+main = print (Chapter2.Even.even 10)
