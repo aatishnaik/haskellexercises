5c924a9690d57788a9b63e6203583f844685a649
aatish <aatish@vacationlabs.com>
2018-01-02 13:33:27 +0530

    signature added

--- src/Chapter2/Isbn.hs
+++ src/Chapter2/Isbn.hs
@@ -4,9 +4,11 @@ import Data.List
 
 rmdash :: String -> String
 rmdash str = filter (/='-') str
+cformat :: String -> Bool
 cformat str = if (False `elem` (map (\c -> (isDigit c)) (rmdash str))) || (length (rmdash str) /= 10)
     then False
     else True
+checkisbn :: String -> Bool
 checkisbn str = if (cformat str)
     then if ((foldl' (\val c -> (val+c)) 0 (map (\c -> (digitToInt c)) (rmdash str))) `mod` 11) == 0
         then True
@@ -14,9 +16,9 @@ checkisbn str = if (cformat str)
     else False
 
 --using zip
+checkisbn2 :: String -> Bool
 checkisbn2 str = if (cformat str)
     then if ((sum (zipWith (*) [1..10] (map (\c -> (digitToInt c)) (rmdash str)))) `mod` 11) == 0
         then True
-
         else False
     else False
