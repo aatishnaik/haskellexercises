aaf45959e3e8a1702a5e8c12848e23a395dd20b1
aatish <aatish@vacationlabs.com>
2018-01-02 17:32:56 +0530

    contact fix

--- src/Chapter2/Contact.hs
+++ src/Chapter2/Contact.hs
@@ -1,17 +1,19 @@
 
 module Chapter2.Contact where
-import Data.Char (toLower)
-import Data.List (foldl')
-
+import Data.Char
+import Data.List
 {-groupNamesByAlphabet :: Char -> [String] -> (Char, [String])
 groupNamesByAlphabet alpha names =
     foldl' (\ (alphabet, collectedNames) name ->
     if (length name > 0) && (toLower (head name) == (toLower alphabet))
             then (alphabet, collectedNames ++ [name])
-            else (alphabet, collectedNames)) (alpha, []) names-}
+            else (alphabet, collectedNames)) (alpha, []) names
     
 groupNamesByAllAlphabets :: [String] -> [(Char, [String])]
-groupNamesByAllAlphabets names = map (\alphabet -> groupNamesByAlphabet alphabet names) ['a'..'z']
+groupNamesByAllAlphabets names = map (\alphabet -> groupNamesByAlphabet alphabet names) ['a'..'z']-}
+--groupAll :: [String] -> [String]
+
+groupAll str = groupBy (\ a b -> ((head a) == (head b))) (sort str)
 
 main :: IO()
-main = print (groupNamesByAllAlphabets ["abc","bbc"])
+main = print (groupAll ["abc","bbc","abab","aaaa"])
