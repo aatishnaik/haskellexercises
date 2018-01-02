dbbbde341d1f498780fe1f2f3593bc65d75152d6
aatish <aatish@vacationlabs.com>
2018-01-02 15:11:13 +0530

    isbn fix not complete

--- src/Chapter2/Isbn.hs
+++ src/Chapter2/Isbn.hs
@@ -5,20 +5,17 @@ import Data.List
 rmdash :: String -> String
 rmdash str = filter (/='-') str
 cformat :: String -> Bool
-cformat str = if (False `elem` (map (\c -> (isDigit c)) (rmdash str))) || (length (rmdash str) /= 10)
-    then False
-    else True
+cformat str = (not) (False `elem` (map (\c -> (isDigit c)) (rmdash str))) || (length (rmdash str) /= 10)
 checkisbn :: String -> Bool
 checkisbn str = if (cformat str)
-    then if ((foldl' (\val c -> (val+c)) 0 (map (\c -> (digitToInt c)) (rmdash str))) `mod` 11) == 0
-        then True
-        else False
+    then ((foldl' (\val c -> (val+c)) 0 (map (\c -> (digitToInt c)) (rmdash str))) `mod` 11) == 0
     else False
 
 --using zip
 checkisbn2 :: String -> Bool
 checkisbn2 str = if (cformat str)
-    then if ((sum (zipWith (*) [1..10] (map (\c -> (digitToInt c)) (rmdash str)))) `mod` 11) == 0
-        then True
-        else False
+    then ((sum (zipWith (*) [1..10] (map (\c -> (digitToInt c)) (rmdash str)))) `mod` 11) == 0
     else False
+
+main :: IO()
+main = print (checkisbn2 "3-598-21508-8")
