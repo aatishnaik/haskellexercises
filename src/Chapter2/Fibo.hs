862121ca53d7211c00fe5649371b5f49091d379e
aatish <aatish@vacationlabs.com>
2018-01-03 14:28:34 +0530

    fibo fix

--- src/Chapter2/Fibo.hs
+++ src/Chapter2/Fibo.hs
@@ -2,10 +2,8 @@ module Chapter2.Fibo where
 import Data.Char
 import Data.List
 
-fibo :: Integer -> Integer
-fibo 0 = 0
-fibo 1 = 1
-fibo n = fibo (n-1) + fibo (n-2)
+fibo :: Int -> (Int,Int,[Int])
+fibo n = foldl' (\ (x,y,list) i -> (y,x+y,list ++ [x+y])) (0,1,[]) [1..n]
 
-efibsum :: Integer -> Integer
-efibsum n = foldl' (\ttl x -> if ((x `mod` 2) == 0) then (ttl + x) else ttl) 0 (map fibo [1..n])
+main :: IO()
+main = print (fibo 10)
--- src/Chapter2/Fibo2.hs
+++ src/Chapter2/Fibo2.hs
@@ -5,6 +5,7 @@ fibo n = foldl' (\ (x,y,list) i -> (y,x+y,list ++ [x+y])) (0,1,[]) [1..n]
 evenf :: Int -> (Int,Int,[Int]) -> Int
 evenf n (x,y,z) = let ch = z
     in sum (filter (\c -> c `mod` 2 == 0) ch)
+evenfib :: Int -> Int
 evenfib n = evenf n (fibo n)
 
 main :: IO()
