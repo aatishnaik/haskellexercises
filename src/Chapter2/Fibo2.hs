3058fb8dd3adaa263afef198bb572d35ce6ff761
aatish <aatish@vacationlabs.com>
2018-01-03 14:02:47 +0530

    fibo fix

--- src/Chapter2/Fibo2.hs
+++ src/Chapter2/Fibo2.hs
@@ -1,13 +1,11 @@
 import Data.List
-evenfib :: Int -> Int
-evenfib n = let ch = changetype (fibo n)
-    in sum (filter (\c -> c `mod` 2 == 0) ch)
-
-changetype :: (Int,Int,[Int]) -> [Int]
-changetype (x,y,z) = z
-
 fibo :: Int -> (Int,Int,[Int])
 fibo n = foldl' (\ (x,y,list) i -> (y,x+y,list ++ [x+y])) (0,1,[]) [1..n]
+
+evenf :: Int -> (Int,Int,[Int]) -> Int
+evenf n (x,y,z) = let ch = z
+    in sum (filter (\c -> c `mod` 2 == 0) ch)
+evenfib n = evenf n (fibo n)
 
 main :: IO()
 main = print(evenfib 10)
