ee265a7b698a62f3a146308650beff6049fbac0e
aatish <aatish@vacationlabs.com>
2018-01-03 14:29:52 +0530

    delete

--- src/Chapter2/ContactList.hs
+++
@@ -1,9 +0,0 @@
-module Chapter2.ContactList where
-import Data.Char
-import Data.List
-
-groupNamesByAlphabet :: Char -> [String] -> (Char, [String])
-groupNamesByAlphabet alpha names = foldl' (\ (alphabet, collectedNames) name ->
-      if ((length name) > 0) && (toLower (head name) == (toLower alphabet))
-      then (alphabet, collectedNames ++ [name])
-      else (alphabet, collectedNames)) (alpha, []) names
