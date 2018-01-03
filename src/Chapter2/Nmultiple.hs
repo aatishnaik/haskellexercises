acda3be7a9226422fd722d5b644b1b943b1d53f4
aatish <aatish@vacationlabs.com>
2018-01-03 13:14:59 +0530

    nmltiple fix

--- src/Chapter2/Nmultiple.hs
+++ src/Chapter2/Nmultiple.hs
@@ -1,16 +1,13 @@
 module Chapter2.Nmultiple where
 import Data.List
 
-check :: (Int,Int,Int) -> Int
-check (x,n,num) = if ((num `mod` 3) == 0) || ((num `mod` 5) == 0)
-    then if x < n
-        then check (x+1,n,num+1)
-        else num
-    else check (x,n,num+1)
+--nmultiples of 3 and 5
 
 nmultiple :: Int -> [Int]
-nmultiple n = map (\x -> check (0,x,0)) [1..n]
+nmultiple n = take n (filter (\n->((n `mod` 3) == 0) || ((n `mod` 5) == 0)) [1..(n*3)])
 
+--sum of multiples till n
 summul :: Int -> Int
 summul n = foldl' (\ttl num -> if ((num `mod` 3) == 0) || ((num `mod` 5) == 0) then (ttl + num) else ttl) 0 [1..n]
-
+main :: IO()
+main = print (nmultiple 10)
