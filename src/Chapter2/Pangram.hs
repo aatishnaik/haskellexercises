f37bfed7f35426ca1f4d2352fa22ac1e7d16dc32
aatish <aatish@vacationlabs.com>
2018-01-04 15:42:04 +0530

    pangram with single any

--- src/Chapter2/Pangram.hs
+++ src/Chapter2/Pangram.hs
@@ -7,10 +7,12 @@ pangram str = (foldl' (\alphabets c -> delete (toLower c) alphabets) ['a'..'z'] 
 
 pangram2 :: String -> Bool
 pangram2 str = (foldl' (\test c -> test ++ (filter (==c) ['a'..'z'])) [] (toLow str)) == (filter (/=' ') (toLow str))
-
+ 
+toLow :: String -> String
 toLow str = map (toLower) str
 
 pangram3 :: String -> Bool
-pangram3 str = (not)(any (False==) (map (\c -> (any (c==) (toLow str))) ['a'..'z']))
+pangram3 str = foldl' (\key c -> if (any (c==) (toLow str))==False then False else True) True ['a'..'z']
+
 main :: IO()
-main = print (pangram "abcdefghijklmnopqrstuvwxyz")
+main = print (pangram3 "aabcdefghijklmnopqrstuvwxyZ")
