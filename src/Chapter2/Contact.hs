b5460598b7abb14c5dd22b2e3c08c94af903b6be
aatish <aatish@vacationlabs.com>
2018-01-05 15:09:05 +0530

    contact fix

--- src/Chapter2/Contact.hs
+++ src/Chapter2/Contact.hs
@@ -2,12 +2,21 @@
 module Chapter2.Contact where
 import Data.Char
 import Data.List
+{-groupNamesByAlphabet :: Char -> [String] -> (Char, [String])
+groupNamesByAlphabet alpha names =
+    foldl' (\ (alphabet, collectedNames) name ->
+    if (length name > 0) && (toLower (head name) == (toLower alphabet))
+            then (alphabet, collectedNames ++ [name])
+            else (alphabet, collectedNames)) (alpha, []) names
+    
+groupNamesByAllAlphabets :: [String] -> [(Char, [String])]
+groupNamesByAllAlphabets names = map (\alphabet -> groupNamesByAlphabet alphabet names) ['a'..'z']-}
 
 groupAll :: [String] -> [[String]]
 groupAll str = groupBy (\ a b -> ((head a) == (head b))) (sort str)
 
---generateArray :: [String] -> (Char,[String])
---generateArray str = 
+generateArray :: [String] -> [(Char,[String])]
+generateArray str = foldl' (\newstr st -> newstr ++ [(head (head st),st)]) [] (groupAll str)
 
 main :: IO()
-main = print (groupAll ["abc","bbc","abab","aaaa"])
+main = print (generateArray ["abc","bbc","abab","aaaa"])
