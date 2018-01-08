5208efdeaa01a500e74ea8aabea24bd2b4a84981
aatish <aatish@vacationlabs.com>
2018-01-08 19:51:40 +0530

    Latestt commit

--- src/Chapter2/ExamScore.hs
+++ src/Chapter2/ExamScore.hs
@@ -1,13 +1,54 @@
 module Chapter2.ExamScore where
 import Data.Char
 import Data.List
---make  new list
---check duplicates in new list
---check subj in subjlist
---check is value lies between 0-100
 
-showRec = let records = [ ("Saurabh Nanda", [("English", 84), ("Chemisty", 80), ("Physics", 95), ("Geography", 75)]), ("John Doe", [("Chemisty", 80), ("Physics", 95), ("Geography", 75)]), ("Jane Doe", [("Chemisty", 66), ("Phsyics", 33), ("Geography", 56)]), ("John Doe", [("Chemisty", 90), ("Economics", 45), ("Geography", 56)]), ("Bahubali", [("Hindi", 45), ("Biology", -90), ("Geography", -75)]), ("Rajnikant", [("Tamil", 110), ("Biology", 100), ("Geography", 100)])]
-    in map (\rec -> rec ) records
+type StudentName = String
+type SubjectName = String
+type SubjectMarks = Int
+type MarkSheet = [(String,[(String,Int)])]
+subAvg :: MarkSheet -> [SubjectName] -> SubjectName -> Int
+subAvg mksheet subjects subname = let subarr = (filter (\x -> (fst x == subname)) (foldl' (\arr x -> arr ++ (snd x)) [] (allValidSubjects mksheet subjects)))
+                in (foldl' (\s x -> (s+(snd x))) 0 subarr) `div` length subarr
+
+checkNames :: MarkSheet -> StudentName -> Bool
+checkNames mksheet studname = length (filter (\x -> (x == studname)) (foldl' (\arr x -> arr ++ [fst x]) [] mksheet)) == 1
+
+checkScore :: MarkSheet -> [SubjectName] -> StudentName -> SubjectName -> (Bool,String)
+checkScore mksheet subjects studname subname = let mark = snd (head (filter (\x -> (fst x == subname)) (snd (head (filter (\x -> (fst x) == studname) mksheet)))))
+                                                    in if (mark < 0)
+                                                        then (False,"negative score")
+                                                        else if (mark > 100)
+                                                        then (False,"greater than 100")
+                                                        --checksubject
+                                                        else (True,"Valid")
+                                                        
+calculateSd :: MarkSheet -> [SubjectName] -> SubjectName -> Int
+calculateSd mksheet subjects subname = let avg = subAvg mksheet subjects subname
+                                           subarr = (filter (\x -> (fst x == subname)) (foldl' (\arr x -> arr ++ (snd x)) [] (allValidSubjects mksheet subjects)))
+                                            in (sum (foldl' (\arr x -> arr ++ [avg - (snd x)]) [] subarr)) `div` (length subarr)
+
+duplicateNames :: MarkSheet -> [StudentName]
+duplicateNames mksheet = let arr = (foldl' (\arr x -> arr ++ [fst x]) [] mksheet)
+                            in (filter (\x -> (not (checkNames mksheet x))) arr)
+
+--invalidSubjects :: MarkSheet -> [SubjectName] -> StudentName -> [(StudentName,SubjectMarks,String)]
+invalidSubjects mksheet subjects studname = foldl' (\arr x -> if (fst (checkScore mksheet subjects studname (fst x)))== False then arr ++ [(fst x,snd x,snd (checkScore mksheet [] studname (fst x)))] else arr) [] (snd (head (filter (\x -> (fst x)== studname) mksheet)))
+--validSubjects :: MarkSheet -> [SubjectName] -> StudentName -> [(StudentName,SubjectMarks)]
+validSubjects mksheet subjects studname = foldl' (\arr x -> if (fst (checkScore mksheet subjects studname (fst x)))== True then arr ++ [(fst x,snd x)] else arr) [] (snd (head (filter (\x -> (fst x)== studname) mksheet)))
+
+allValidSubjects :: MarkSheet -> [SubjectName] -> MarkSheet
+allValidSubjects mksheet subjects = foldl' (\arr x -> arr ++ [((fst x),(validSubjects mksheet subjects (fst x)) )]) [] mksheet
+
+--invalidEntries :: MarkSheet -> [SubjectName] -> [(StudentName,[(SubjectName,SubjectMarks,String)])]
+invalidEntries mksheet subjects = foldl' (\arr x -> 
+    if (not (checkNames mksheet (fst x)))
+        then arr ++ [(fst x,[("",0,"name duplicated")])]
+    else if ((invalidSubjects mksheet subjects (fst x)) /= [])
+        then arr ++ [(fst x,(invalidSubjects mksheet subjects (fst x)))]
+    else arr) [] mksheet
+
+-- studentsInExam :: MarkSheet -> [SubjectName] -> [(SubjectName,[StudentName])]
+-- studentsInExam = _todo
 
 main :: IO()
-main = print showRec
+main = print (calculateSd [("Saurabh Nanda", [("English", 84), ("Chemisty", 80), ("Physics", 95), ("Geography", 75)]), ("John Doe", [("Chemisty", 80), ("Physics", 95), ("Geography", 75)]), ("Jane Doe", [("Chemisty", 66), ("Phsyics", 33), ("Geography", 56)]), ("John Doe", [("Chemisty", 90), ("Economics", 45), ("Geography", 56)]), ("Bahubali", [("Hindi", 45), ("Biology", -90), ("Geography", -75)]), ("Rajnikant", [("Tamil", 110), ("Biology", 100), ("Geography", 100)])] [] "Geography")
--- src/Chapter2/GenFibo.hs
+++ src/Chapter2/GenFibo.hs
@@ -2,5 +2,20 @@ module Chapter2.GenFibo where
 import Data.Char
 import Data.List
 
+genFibo :: (Int -> Bool) -> [Int]
+getFibo con = _todo
+
+getarr :: (Int,Int,[Int]) -> [Int ]
+getarr (n1,n2,arr) = arr
+
+fibo :: Int -> (Int,Int,[Int])
+fibo n = foldl' (\ (x,y,list) i -> (y,x+y,list ++ [x+y])) (0,1,[]) [1..(n*10)]
+
+neven :: Int -> [Int]
+neven n = take n (filter (\n->((n `mod` 2) == 0)) (getarr (fibo n)))
+
+ntri :: Int -> [Int]
+ntri n = take n (filter (\n->((n `mod` 3) == 0)) (getarr (fibo n)))
+
 main :: IO()
 main = print ""
