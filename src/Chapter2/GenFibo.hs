2c0d889c76a1f7e7fbdc652f04ba7de0a9024c45
aatish <aatish@vacationlabs.com>
2018-01-11 10:57:22 +0530

    GenFibo done

--- src/Chapter2/GenFibo.hs
+++ src/Chapter2/GenFibo.hs
@@ -2,25 +2,20 @@ module Chapter2.GenFibo where
 import Data.Char
 import Data.List
 
-genFibo :: Int -> Int -> [Int]
-genFibo = \x -> \n -> 
-    if x == 2
-        then neven n
-    else if x == 3
-        then ntri n
-    else []
-
 getarr :: (Int,Int,[Int]) -> [Int ]
 getarr (n1,n2,arr) = arr
 
 fibo :: Int -> (Int,Int,[Int])
 fibo n = foldl' (\ (x,y,list) i -> (y,x+y,list ++ [x+y])) (0,1,[]) [1..(n*10)]
 
+genFibo :: (Int -> Bool) -> Int -> [Int]
+genFibo fn n =  (take n (filter (\num -> fn num) (getarr (fibo n))))
+
 neven :: Int -> [Int]
-neven n = take n (filter (\n->((n `mod` 2) == 0)) (getarr (fibo n)))
+neven n = genFibo (\x -> (x `mod` 2) == 0) n
 
 ntri :: Int -> [Int]
-ntri n = take n (filter (\n->((n `mod` 3) == 0)) (getarr (fibo n)))
+ntri n = genFibo (\x -> (x `mod` 3) == 0) n
 
 main :: IO()
-main = print (genFibo 2 5)
+main = print (neven 10)
--- src/Chapter2/genFibo2.hs
+++ src/Chapter2/genFibo2.hs
@@ -6,16 +6,19 @@ fibo :: Int -> Int
 fibo 0 = 0
 fibo 1 = 1
 fibo n = fibo (n-1) + fibo (n-2)
+narr :: Int -> Int -> [Int]
 narr num n
     |num < n = [fibo num] ++ (narr (num+1) n)
     |num == n = [fibo num]
 
-neven2 num n arr
-    |num <= n = if ((head arr) `mod` 2) == 0
-                then [head arr] ++ (neven2 (num+1) n (tail arr))
-                else neven2 (num) n (tail arr)
-    |num > n = []
+genFibo :: (Int -> Bool) -> Int -> [Int]
+genFibo fn n =  (take n (filter (\num -> fn num) (narr 1 (n*10))))
 
-neven n = neven2 1 n (narr 1 (n*10))
+neven :: Int -> [Int]
+neven n = genFibo (\x -> (x `mod` 2) == 0) n
+
+ntri :: Int -> [Int]
+ntri n = genFibo (\x -> (x `mod` 3) == 0) n
+
 main :: IO()
 main = print (neven 10)
