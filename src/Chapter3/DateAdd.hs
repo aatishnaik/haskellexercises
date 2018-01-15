ff156f69db39b1bfcd7d846376ed00330f834ade
aatish <aatish@vacationlabs.com>
2018-01-15 18:28:29 +0530

    part 1 done

--- src/Chapter3/DateAdd.hs
+++ src/Chapter3/DateAdd.hs
@@ -3,6 +3,7 @@ import Data.List
 import Data.Char
 
 --version1
+{-
 addDays :: (Int, Int, Int) -> Int -> (Int, Int, Int)
 addDays (day,month,year) dys = 
     let tdays = day+dys
@@ -17,7 +18,7 @@ addDays (day,month,year) dys =
         if (month + (tdays `div`30)) > 12 
             then year+(((month + (tdays `div`30)) `mod` 12)+1)
             else year)
-
+-}
 
 --version2
 data Day = MkDay Int deriving Show
@@ -28,13 +29,37 @@ addDays2 :: (Day, Month, Year) -> Day -> (Day, Month, Year)
 addDays2 (day,month,year) dys =
     let tdays = day+dys
         monthlist = if (year `mod` 4) == 0 then [31,29,31,30,31,30,31,31,30,31,30,31] else [31,28,31,30,31,30,31,31,30,31,30,31]
-        xmdays = tdays `div` 30
+        (xmon,xday) = divMod tdays 30
         in(
-        if tdays <= (monthlist!!month) then tdays else tdays `mod` 30,
-        if (tdays > (monthlist!!month)) && ((month + ) <= 12) then month + xmdays
-            else if ((day+dys) > 30) && ((month + xmdays) > 12)
-                then ((month + xmdays) `mod` 12)
+            if tdays <= (monthlist!!month) then tdays else tdays `mod` 30,
+            if (tdays > (monthlist!!month)) && ((month + xday) <= 12) then month + xday
+                else if ((day+dys) > 30) && ((month + xday) > 12)
+                    then ((month + xday) `mod` 12)
+                else month,
+            if (month + xmdays) > 12 
+                then year+((month + xday) `mod` 12)
+                else year)
+--version3
+{-
+data Date = MkDate {
+  day :: Int
+, month :: Int
+, year :: Int
+} deriving (Eq, Show, Ord)
+
+add :: Date -> Int -> Date
+add (MkDate {day = d, month = m, year = y}) dys = 
+    let tdays = d+dys
+    in MkDate
+    {
+        day = if (d+dys) < 30 then (d+dys) else (d+dys `mod` 30),
+        month = 
+            if (tdays > 30) && (month + (tdays `div` 30)) < 12 then month + (tdays `div`30)
+            else if ((d+dys) > 30) && (month + (tdays `div`30)) > 12 
+                then ((month + (tdays `div`30)) `mod` 12)
             else month,
-        if (month + xmdays) > 12 
-            then year+((month + xmdays) `mod` 12)
-            else year)
+        year = if (month + (tdays `div`30)) > 12 
+            then y+(month + (tdays `div`30) `mod` 12)
+            else year
+    }
+    -}
