d67b550a7ce3842439e517f6985190f4b7eb2044
aatish <aatish@vacationlabs.com>
2018-01-16 16:54:29 +0530

    not happening.

--- src/Chapter3/ExamScore.hs
+++ src/Chapter3/ExamScore.hs
@@ -3,11 +3,12 @@ import Data.Char
 import Data.List
         
 data StudentName = MkStudentName String deriving Show
-data SubjectName = MkSSubjectName String deriving Show
+data SubjectName = MkSubjectName String deriving Show
 data SubjectMarks = MkSubjectMarks Int deriving Show
-data MarkSheet = MkMarksheet [(StudentName,[(SubjectName,SubjectMarks)])] deriving Show
-    
+data MarkSheet = MkMarkSheet [(StudentName,[(SubjectName,SubjectMarks)])] deriving Show
+
 --subAvg :: MarkSheet -> [SubjectName] -> SubjectName -> Float
-subAvg mksheet subjects subname =
-    let [(sname,score)] = mksheet
+
+subAvg (MkMarkSheet [(MkStudentName studname,[(MkSubjectName subjname,MkSubjectMarks subjmarks)])]) (MkSubjectName subname) =
+    let sname = map (\(s,scr) -> s) [(studname,[(subjname,subjmarks)])]
     in sname
