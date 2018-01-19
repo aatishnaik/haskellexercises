75e322938c0fe69cc7ad92ad71ccdf021d59fa07
aatish <aatish@vacationlabs.com>
2018-01-19 17:27:59 +0530

    constructor fixes

--- src/Chapter3/ExamScore.hs
+++ src/Chapter3/ExamScore.hs
@@ -9,8 +9,8 @@ data SubjectArr= MkSubjectArr [SubjectName] deriving Show
 data MarkSheet = MkMarkSheet [(StudentName,[(SubjectName,SubjectMarks)])] deriving Show
 
 subAvg :: MarkSheet -> SubjectArr -> SubjectName -> Float
-subAvg (MkMarkSheet mksheet) (MkSubjectArr subjects) (MkSubjectName subname) = 
-    let validSubs = allValidSubjects (MkMarkSheet mksheet) (MkSubjectArr subjects)
+subAvg mksheet subjects (MkSubjectName subname) = 
+    let validSubs = allValidSubjects mksheet subjects
         subs = map (\x -> (snd x)) validSubs
         subarr = map (\x -> (filter (\(MkSubjectName namesb,markssub) -> namesb == subname) x )) subs
         scoresarr = concat subarr
@@ -18,23 +18,25 @@ subAvg (MkMarkSheet mksheet) (MkSubjectArr subjects) (MkSubjectName subname) =
         tarr = map (\(MkSubjectMarks x)-> x) scores
     in fromIntegral (sum tarr) / fromIntegral(length tarr)
 
+
 filterSubjects :: Bool -> MarkSheet -> SubjectArr -> StudentName -> [(SubjectName,SubjectMarks,String)]
-filterSubjects filterFn (MkMarkSheet markSheet) (MkSubjectArr subjects) (MkStudentName studName) = 
+filterSubjects filterFn (MkMarkSheet markSheet) subjects (MkStudentName studName) = 
     let subs = filter (\ (MkStudentName studentName, scoreList) -> studentName == studName) markSheet
         subjectList = snd (head subs)
-        validSubs = map (\x -> (checkScore (MkMarkSheet markSheet) (MkSubjectArr subjects) (MkStudentName studName) (fst x))) subjectList
+        validSubs = map (\x -> (checkScore (MkMarkSheet markSheet) subjects (MkStudentName studName) (fst x))) subjectList
         list = filter (\ (namesub,markssub,flag,msg) -> (filterFn == flag)) validSubs
     in map (\ (namesub,markssub,flag,msg) -> (namesub,markssub,msg)) list
 
 allInvalidSubjects :: MarkSheet -> SubjectArr -> [(StudentName,[(SubjectName,SubjectMarks,String)])]
-allInvalidSubjects (MkMarkSheet markSheet) (MkSubjectArr subjects) =
-    let invalidNames = map (\x -> (fst x,(filterSubjects False (MkMarkSheet markSheet) (MkSubjectArr subjects) (fst x)))) markSheet
+allInvalidSubjects (MkMarkSheet markSheet) subjects =
+    let invalidNames = map (\x -> (fst x,(filterSubjects False (MkMarkSheet markSheet) subjects (fst x)))) markSheet
     in invalidNames
 
 allValidSubjects :: MarkSheet -> SubjectArr -> [(StudentName,[(SubjectName,SubjectMarks)])]
-allValidSubjects (MkMarkSheet markSheet) (MkSubjectArr subjects) =
-    let validNames = map (\x -> (fst x,(filterSubjects True (MkMarkSheet markSheet) (MkSubjectArr subjects) (fst x)))) markSheet
+allValidSubjects (MkMarkSheet markSheet) subjects =
+    let validNames = map (\x -> (fst x,(filterSubjects True (MkMarkSheet markSheet) subjects (fst x)))) markSheet
     in map (\(namestud,score) -> (namestud,(map (\(namesub,markssub,msg) -> (namesub,markssub)) score))) validNames
+
 
 checkScore :: MarkSheet -> SubjectArr -> StudentName -> SubjectName -> (SubjectName,SubjectMarks,Bool,String)
 checkScore (MkMarkSheet mksheet) (MkSubjectArr subjects) (MkStudentName studname) (MkSubjectName subname) =  
@@ -50,6 +52,7 @@ checkScore (MkMarkSheet mksheet) (MkSubjectArr subjects) (MkStudentName studname
         then (MkSubjectName subname,MkSubjectMarks mark,False,"invalid subject name")
         else (MkSubjectName subname,MkSubjectMarks mark,True,"Valid")
 
+
 filterNames :: ([String] -> Bool) -> MarkSheet -> [StudentName]
 filterNames filterFn (MkMarkSheet markSheet) =
     let studentNames = map (\ (MkStudentName studentName, scoreList) -> studentName) markSheet
@@ -58,15 +61,15 @@ filterNames filterFn (MkMarkSheet markSheet) =
     in map (\x -> (MkStudentName (head x))) filterGroups
     
 uniqueNames :: MarkSheet -> [StudentName]
-uniqueNames (MkMarkSheet markSheet) = filterNames (\x -> length x == 1) (MkMarkSheet markSheet)
+uniqueNames markSheet = filterNames (\x -> length x == 1) markSheet
         
 duplicateNames :: MarkSheet -> [StudentName]
-duplicateNames (MkMarkSheet markSheet) = filterNames (\x -> length x > 1) (MkMarkSheet markSheet)
+duplicateNames markSheet = filterNames (\x -> length x > 1) markSheet
 
 calculateSd :: MarkSheet -> SubjectArr -> SubjectName -> Float 
-calculateSd (MkMarkSheet mksheet) (MkSubjectArr subjects) (MkSubjectName subname) =
-    let avg = subAvg (MkMarkSheet mksheet) (MkSubjectArr subjects) (MkSubjectName subname)
-        subScores = map (\x -> snd x) (allValidSubjects (MkMarkSheet mksheet) (MkSubjectArr subjects))
+calculateSd mksheet subjects (MkSubjectName subname) =
+    let avg = subAvg mksheet subjects (MkSubjectName subname)
+        subScores = map (\x -> snd x) (allValidSubjects mksheet subjects)
         subarr = map (\x -> (filter (\(MkSubjectName namesub,MkSubjectMarks markssub) -> namesub == subname) x )) subScores
         mksArr = map (\(MkSubjectName x,MkSubjectMarks y) -> (fromIntegral y)-avg) (concat subarr)
         squareArr = map (\x -> (x*x)) mksArr
@@ -74,14 +77,14 @@ calculateSd (MkMarkSheet mksheet) (MkSubjectArr subjects) (MkSubjectName subname
     in sqrt variance
 
 allValidNames :: MarkSheet -> SubjectArr -> [(StudentName,[(SubjectName,SubjectMarks)])]
-allValidNames (MkMarkSheet mksheet) (MkSubjectArr subjects) =
-    let newmksheet = allValidSubjects (MkMarkSheet mksheet) (MkSubjectArr subjects)
-        duplicateArr = map (\(MkStudentName x) -> x) (duplicateNames (MkMarkSheet mksheet))
+allValidNames mksheet subjects =
+    let newmksheet = allValidSubjects mksheet subjects
+        duplicateArr = map (\(MkStudentName x) -> x) (duplicateNames mksheet)
     in filter (\(MkStudentName x,y) -> x `notElem` duplicateArr) newmksheet
 
 studentsInSubject :: MarkSheet -> SubjectArr -> [(String,[String])]
-studentsInSubject (MkMarkSheet mksheet) (MkSubjectArr subjects) =
-    let vList = allValidNames (MkMarkSheet mksheet) (MkSubjectArr subjects)
+studentsInSubject mksheet (MkSubjectArr subjects) =
+    let vList = allValidNames mksheet (MkSubjectArr subjects)
         newlist = map (\(MkStudentName x,y) -> (x,(map (\(MkSubjectName namesub,MkSubjectMarks mkssub) -> namesub) y))) vList
         subArr = map (\(MkSubjectName x)->x) subjects
         arr = map (\s -> (s,(foldl' (\arr (namestud,namesub) -> 
@@ -92,12 +95,13 @@ studentsInSubject (MkMarkSheet mksheet) (MkSubjectArr subjects) =
     in arr
 
 subjectsInExam :: MarkSheet -> SubjectArr -> [([String],[String])]
-subjectsInExam  (MkMarkSheet mksheet) (MkSubjectArr subjects) =
-    let namelist = map (\(MkStudentName namestud,scorelist) -> (namestud,(map (\(MkSubjectName namesub,MkSubjectMarks mkssub) -> namesub) scorelist))) (allValidNames (MkMarkSheet mksheet) (MkSubjectArr subjects))
-        sublist = studentsInSubject (MkMarkSheet mksheet) (MkSubjectArr subjects)
+subjectsInExam  mksheet subjects =
+    let namelist = map (\(MkStudentName namestud,scorelist) -> (namestud,(map (\(MkSubjectName namesub,MkSubjectMarks mkssub) -> namesub) scorelist))) (allValidNames mksheet subjects)
+        sublist = studentsInSubject mksheet subjects
         newname = delete [] (nub (map (\(x,y) -> y) namelist))
         newsub =  delete [] (nub (map (\(x,y) -> y) sublist))
         emptyele = filter (\(x,y)-> y == []) sublist
         emptylist = map (\(x,y) -> ([x],y)) emptyele
         newlist = zip newname newsub
     in newlist ++ emptylist
+
