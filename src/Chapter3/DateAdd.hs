134563385f03db50c21a05cd6cb64aec707c175b
aatish <aatish@vacationlabs.com>
2018-01-30 18:03:55 +0530

    date Addition done properly.

--- src/Chapter3/DateAdd.hs
+++ src/Chapter3/DateAdd.hs
@@ -1,22 +1,36 @@
 module Chapter3.DateAdd where
 import Data.List
 import Data.Char
+checkLeap :: Int -> Bool
+checkLeap yr = if (((yr `mod` 100) /= 0) && ((yr `mod` 4) == 0)) || ((yr `mod` 400) == 0)
+        then True
+        else False
 
-addDays :: (Int, Int, Int) -> Int -> (Int, Int, Int)
-addDays (day,month,year) dys = 
-    let tdays = day+dys
-        monthlist = if (year `mod` 4) == 0 then [31,29,31,30,31,30,31,31,30,31,30,31] else [31,28,31,30,31,30,31,31,30,31,30,31]
-        marr = [month..((month + (tdays `div` 31)))]
-        msum = fromIntegral (sum (map (\x->(monthlist!!(x-1))) marr))
-        len = (fromIntegral (length marr))
-        avg = msum `div` len
-    in(
-        if tdays <= (monthlist!!(month-1)) then tdays else (tdays `mod` avg)+1,
-        if (tdays > (monthlist!!(month-1))) && ((month + (tdays `div` avg)) <= 12) 
-            then month + (tdays `div` avg)
-            else if ((day+dys) >(monthlist!!(month-1))) && ((month + (tdays `div` avg)) > 12)
-                then ((month + (tdays `div`avg)) `mod` 12)+1
-            else month,
-        if (month + (tdays `div`avg)) > 12 
-            then year+(((month + (tdays `div` avg)) `mod` 12)+1)
-            else year)
+getOffset :: Int -> Int -> Int -> Int
+getOffset d m y =
+    let monthlist = if checkLeap y then [31,29,31,30,31,30,31,31,30,31,30,31] else [31,28,31,30,31,30,31,31,30,31,30,31]
+    in d + (sum (take (m-1) monthlist))
+
+
+addDays :: Int -> Int -> Int -> Int -> (Int,Int,Int)
+addDays d m y x = 
+    let offset = (getOffset d m y) + x
+        (y2,mOffset) = addYr y offset
+        (m2,dOffset) = addMon y2 mOffset
+    in (dOffset,m2,y2)
+
+addMon :: Int -> Int -> (Int,Int)
+addMon yr offset =
+    let monthlist = if checkLeap yr then [31,29,31,30,31,30,31,31,30,31,30,31] else [31,28,31,30,31,30,31,31,30,31,30,31]
+    in foldl' (\(month,off) mday ->
+            if (off-mday) > 0
+                then (month+1,off-mday)
+            else (month,off)
+        ) (1,offset) monthlist
+
+addYr :: Int -> Int -> (Int,Int)
+addYr yr offset =
+    let yrd = if checkLeap yr then 366 else 365
+    in if (offset-yrd) > 0
+        then addYr (yr+1) (offset-yrd)
+        else (yr,offset)
