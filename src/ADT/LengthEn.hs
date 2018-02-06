9fe63e5abbe368ec59a590ae41d789549b01b4ae
aatish <aatish@vacationlabs.com>
2018-02-06 17:48:05 +0530

    last problem. including number case handled.

--- src/ADT/LengthEn.hs
+++ src/ADT/LengthEn.hs
@@ -1,2 +1,35 @@
 module ADT.LengthEncoding where
+import Data.List
 
+data Expr = EChar Int Char Expr Expr
+    | Last
+    deriving (Eq, Show, Ord)
+
+lengthEncode :: String -> String
+lengthEncode str = showlist (encodeLength Last str) ""
+
+encodeLength :: Expr -> String -> Expr
+encodeLength e inputStr = 
+    let inp = group inputStr
+        (count,ch) = if (length inp) > 0 then (length (head inp),head (head inp)) else (0,'x')
+        remString = concat (tail inp)
+    in case e of
+        EChar _ _ nxt _-> 
+            if count == 0
+                then e
+                else encodeLength (EChar count ch nxt e) remString
+        Last -> encodeLength (append e (EChar count ch Last e)) remString
+
+append :: Expr -> Expr -> Expr
+append e value = case e of 
+    EChar num val nxt _ -> EChar num val (append nxt value) e
+    Last -> 
+        let EChar n v _ _= value
+        in EChar n v Last e
+
+showlist :: Expr -> String -> String
+showlist e arr= case e of 
+    EChar c v _ prev -> 
+        let str = if c > 1 then (show c)++[v]++" "++ arr else [v]++arr
+        in showlist prev str
+    Last -> arr
