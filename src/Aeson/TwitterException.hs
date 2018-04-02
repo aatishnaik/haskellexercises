41a255ec4d8b81b7515251b451fc66aadc7e6aee
aatish <aatish@vacationlabs.com>
2018-04-02 18:04:19 +0530

    changes made.

--- src/Aeson/TwitterException.hs
+++ src/Aeson/TwitterException.hs
@@ -12,6 +12,8 @@ import qualified Data.ByteString.Char8 as DB
 import qualified Data.ByteString.Lazy as BL
 import Prelude hiding (id)
 import Control.Exception
+import Data.List.Split as DLS
+import Data.String.Conv as DSC
 
 resGetFollowers :: String
 resGetFollowers = "https://api.twitter.com/1.1/friends/list.json"
@@ -25,94 +27,99 @@ resfollow = "https://api.twitter.com/1.1/friendships/create.json"
 resverify :: String
 resverify = "https://api.twitter.com/1.1/users/show.json"
 
-data Follower = Follower {
-    id :: Integer,
-    name :: String ,
-    screen_name :: String,
-    followers_count :: Int
+type UserId = Integer
+type UserName = String
+type UserScreenName = String
+type UserFollowerCount = Integer
+
+data User = User {
+    id :: UserId,
+    name :: UserName ,
+    screenName :: UserScreenName,
+    followersCount :: UserFollowerCount
 }deriving (Show,Eq,Generic)
 
-instance FromJSON Follower where
-    parseJSON (Object v) = 
-        do
-        uid <- v .: "id"
-        uname <- v .: "name"
-        uscreen_name <- v .: "screen_name"
-        ufollowers_count <- v .: "followers_count"
-        pure Follower {
-            id=uid,
-            name=uname,
-            screen_name=uscreen_name,
-            followers_count=ufollowers_count
-        }
-    parseJSON _ = error "Invalid Input"
+instance FromJSON User where
+    parseJSON =  
+        withObject "User" $ \v ->  do
+            uid <- v .: "id"
+            uname <- v .: "name"
+            uscreenName <- v .: "screen_name"
+            ufollowersCount <- v .: "followers_count"
+            pure User {
+                id=uid,
+                name=uname,
+                screenName=uscreenName,
+                followersCount=ufollowersCount
+            }
 
-data FollowerList = FollowerList {
-    followerList :: [Follower]
+data UserList = UserList {
+    userList :: [User]
 }deriving (Show,Eq,Generic)
 
-instance FromJSON FollowerList where
-    parseJSON (Object v) = 
-        do
+instance FromJSON UserList where
+    parseJSON = 
+        withObject "User" $ \v ->  do
             userlst <- v .: "users"
-            pure (FollowerList {followerList = userlst})
-    parseJSON _ = error "Invalid Input"
+            pure UserList {userList = userlst}
 
 authenticator :: Options
 authenticator =
     let 
-        authKey = oauth1Auth (DB.pack "FaPdykltzoxz65Tf9TH9ugIe4") (DB.pack "4yxJAXPQMQinymZI6CDgWNhMdLAT68zjucqvlpNeS7glDvSEtc") (DB.pack "976334890915397632-DpsHhYJiTKkQY7ip48JFcqK86xbrsBP") (DB.pack "a6C63vZ8sQmFFgQjm1KnRy4VYPKwRy1dfNcFb5nxliYMH")
+        authKey = oauth1Auth ((DSC.toS ("FaPdykltzoxz65Tf9TH9ugIe4"::String))::DB.ByteString) ((DSC.toS ("4yxJAXPQMQinymZI6CDgWNhMdLAT68zjucqvlpNeS7glDvSEtc"::String))::DB.ByteString) ((DSC.toS ("976334890915397632-DpsHhYJiTKkQY7ip48JFcqK86xbrsBP"::String))::DB.ByteString) ((DSC.toS ("a6C63vZ8sQmFFgQjm1KnRy4VYPKwRy1dfNcFb5nxliYMH"::String))::DB.ByteString)
         --def = set NW.checkResponse (Just $ \_ _ -> return ()) defaults
         --opt = def & NW.auth ?~ authKey
         opt = defaults & NW.auth ?~ authKey
     in opt
 
-getFollowers :: String -> IO (Either String FollowerList)
+getFollowers :: String -> IO (Either String UserList)
 getFollowers userName =
     let urlStr = resGetFollowers++"?cursor=-1&screen_name="++userName++"&skip_status=true&include_user_entities=false"
         userData = getWith authenticator urlStr
-    in userData >>= \udata -> pure (eitherDecode (udata ^. responseBody))
+    in do
+        udata <- userData
+        pure (eitherDecode (udata ^. responseBody))
 
 --extracts screen name from list of follower objects
-getScrNames :: (Either String FollowerList) -> [String]
+getScrNames :: (Either String UserList) -> [String]
 getScrNames fList = 
-    let Right FollowerList {followerList = follArr} = fList
-    in DL.map (\Follower{
-        id=_,name=_,screen_name=scrname,followers_count=_
+    let Right UserList {userList = follArr} = fList
+    in DL.map (\User{
+        id=_,name=_,screenName=scrname,followersCount=_
     } -> scrname) follArr
 
-checkFollowers :: [String] -> [String] -> ([String],String)
-checkFollowers ufList fList = 
+splitFollowers :: [String] -> [String] -> ([String],[String])
+splitFollowers ufList fList = 
     let
         fol = ufList `intersect` fList
-        ufol = DL.foldl' (\arr f-> if(f `notElem` fList) then arr++" "++f else arr) "" ufList
+        ufol = DL.foldl' (\arr f-> if(f `notElem` fList) then arr++[f] else arr) [] ufList
     in (fol,ufol)
 
-getUnfollowerIds :: (Either String FollowerList) -> [String] -> [Integer]
+getUnfollowerIds :: (Either String UserList) -> [String] -> [Integer]
 getUnfollowerIds fList ufList= 
-    let Right FollowerList {followerList = follArr} = fList
-    in DL.foldl' (\arr Follower{
-        id=usrid,name=_,screen_name=scrName,followers_count=_
+    let Right UserList {userList = follArr} = fList
+    in DL.foldl' (\arr User{
+        id=usrid,name=_,screenName=scrName,followersCount=_
     } -> if (scrName `elem` ufList) then arr++[usrid] else arr) [] follArr
 
-getUserList :: IO([String])
+getUserList :: IO[String]
 getUserList =
     do
         putStrLn "Enter a delimiter char:"
-        delimiter <-getLine
+        delimiter <- getLine
         putStrLn "Enter User ScreenName list seperated by delimiter:"
-        userList <-getLine
-        pure $ words (map (\c-> if c == (head delimiter) then ' ' else c) userList)
+        usersList <- getLine
+        pure $ splitOn delimiter usersList
 
 unfollowUserList :: IO ()
 unfollowUserList =
     do
         f <- getFollowers "aatishVL"
         usrs <- getUserList
-        (validUsers,invalidUsers) <- pure $ checkFollowers usrs (getScrNames f)
+        (validUsers,invalidUsers) <- pure $ splitFollowers usrs (getScrNames f)
         unfollowIds <- pure (getUnfollowerIds f validUsers)
         ufStr <- pure ("\nSuccessfully Unfollowed :"++
-            (foldl' (\arr u-> arr++" "++u) "" validUsers)++"\nUnable to Unfollow :"++invalidUsers)
+            (foldl' (\arr u-> arr++" "++u) "" validUsers)++"\nUnable to Unfollow :"++(concat invalidUsers))
         CM.mapM (\u -> (postWith authenticator (resunfollow++"?user_id="++(show u)) (DB.pack "SAMPLE TEXT"))) unfollowIds >> putStrLn ufStr
 
 followUserList :: IO ()
@@ -120,14 +127,14 @@ followUserList =
     do
         f <- getFollowers "aatishVL"
         usrs <- getUserList
-        (iUsers,vUsers) <- pure $ checkFollowers usrs (getScrNames f)
+        (iUsers,vUsers) <- pure $ splitFollowers usrs (getScrNames f)
         validUsers <- CM.foldM (\arr u -> checkScrName u >>= \sc ->
             case sc of
                 Right val -> if (val ^. responseStatus ^. statusCode)==200
                     then pure (arr++[u])
                     else pure arr
                 Left _ -> pure arr
-            ) [] (words vUsers)
+            ) [] vUsers
         CM.mapM (\u -> (postWith authenticator (resfollow++"?screen_name="++u++"&follow=true") (DB.pack "SAMPLE TEXT"))) validUsers 
             >> putStrLn ("Are already being followed:"++(foldl' (\arr u-> arr++" "++u) "" iUsers)
             ++"\nNow following:"++(foldl' (\arr u-> arr++" "++u) "" validUsers))
@@ -135,11 +142,8 @@ followUserList =
 checkScrName :: String -> IO (Either SomeException (Response BL.ByteString))--IO (Int)
 checkScrName userName =
     let urlStr = resverify++"?screen_name="++userName
-        userData = try (getWith authenticator urlStr) -- :: IO (Either SomeException (Response BL.ByteString))
-    in userData {->>= \udata -> 
-        case udata of
-            Right val -> pure (val ^. responseStatus ^. statusCode)
-            Left _ -> pure 403-}
+        userData = try (getWith authenticator urlStr)
+    in userData
 
 twitterBot :: IO()
 twitterBot =
